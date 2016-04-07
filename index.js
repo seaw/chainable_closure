f071b28c1ec025b513c315efd4cd0e8c05345182
Chun-Wen Wang <chunwen@gogolook.com>
2016-04-07 14:27:42 +0800

    Add new test case and remove buggy variant

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
 'use strict'
 
-function chainable(current_val) {
-  return (new_arg) =>
-    (new_arg === undefined) ? current_val : chainable(current_val + new_arg);
-}
+var chainable = (current_val) =>
+    (new_arg) =>
+      (new_arg === undefined) ? current_val : chainable(current_val + new_arg)
 
-function closure_maker() {
-  var result = 0;
-
-  return function closure(val) {
-    if (val === undefined) {
-      return result;
-    }
-
-    result += val;
-
-    return closure;
-  };
-}
-
-function sum(init_val) {
-  var closure = closure_maker();
-
-  return closure(init_val);
-}
-
-var variants = {
-  'new closure every time': chainable(0),
-  'reuse closure': sum,
-}
-
-module.exports = variants;
+module.exports = chainable(0);
