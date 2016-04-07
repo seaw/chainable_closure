a5d90a4d286fa12aa16d1aa1ab058a3d39c74f5f
Chun-Wen Wang <chunwen@gogolook.com>
2016-04-07 13:26:46 +0800

    連續呼叫會累計總和

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ function sum(arg) {
     return 0;
   }
 
+  var result = arg;
+
   return function inner(arg2){
     if (arg2 === undefined) {
-      return arg;
+      return result;
     }
+    result += arg2;
     return inner;
   };
 }
