a39e71bb2334e962ab5e1cc117270d7b06ab82ac
Chun-Wen Wang <chunwen@gogolook.com>
2016-04-07 15:06:58 +0800

    Remove unused argument

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function chainable(init, f) {
-  var c = (arg) => (next) => (next === undefined) ? arg : c(f(arg, next), f);
+  var c = (arg) => (next) => (next === undefined) ? arg : c(f(arg, next));
   return c(init);
 }
 
